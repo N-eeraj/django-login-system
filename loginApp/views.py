0c1622056cac2e4abbec2c373f43ca895c424386
N-eeraj <neeraj7rajeena@gmail.com>
2021-09-05 09:43:35 +0530

    login function connected to db

--- loginApp/views.py
+++ loginApp/views.py
@@ -9,22 +9,36 @@ def login(request):
     return render(request, 'index.html')
 
 def sign_in(request):
-    mail = request.POST['mail']
-    pswd = request.POST['pswd']
-    name = 'Name'
-    return render(request, 'home.html', {'name': name})
+
+    try:
+        obj = LoginTable.objects.get(mail=request.POST['mail'])
+
+        if obj.password == request.POST['pswd']:
+            user = obj.name
+            return render(request, 'home.html', {'name': user})
+        
+        return HttpResponse('<h1>Invalid Credentials</h1><a href="..">Back</a>')
+    
+    except:
+        return HttpResponse('<h1>Invalid Credentials</h1><a href="..">Back</a>')
 
 def register(request):
     return render(request, 'register.html')
 
 def sign_up(request):
 
-    new_user = LoginTable()
+    h1 = 'Passwords Not Matching'
+    success = False
 
+    new_user = LoginTable()
     new_user.name = request.POST['name']
     new_user.mail = request.POST['mail']
+
     if request.POST['pswd1'] == request.POST['pswd2']:
         new_user.password = request.POST['pswd1']
         new_user.save()
-        return render(request, 'registration.html')
-    return HttpResponse('<h1>Passwords Not Matching</h1>')
+
+        h1 = 'Registration Successful'
+        success = True
+
+    return render(request, 'registration.html', {'heading': h1, 'success': success})
